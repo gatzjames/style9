987fde090b9a8463e11ad936c2b636b667ffb023
gatz <jamesgatzos@gmail.com>
2021-03-16 18:43:10 +0100

    update webpack loader to work with v5

--- webpack/index.js
+++ webpack/index.js
@@ -1,7 +1,11 @@
 const { SourceMapSource, RawSource } = require('webpack-sources');
+const { version } = require('webpack');
+
 const NAME = require('../package.json').name;
 const processCSS = require('../src/process-css.js');
 const virtualModules = require('./virtualModules.js');
+
+const isWebpack5 = parseInt(version, 10) == 5;
 
 class Style9Plugin {
   constructor({ test = /\.css$/ } = {}) {
@@ -12,35 +16,70 @@ class Style9Plugin {
     virtualModules.apply(compiler);
 
     compiler.hooks.compilation.tap(NAME, compilation => {
-      compilation.hooks.optimizeChunkAssets.tapPromise(NAME, async chunks => {
-        const paths = Array.from(chunks)
-          .flatMap(chunk => Array.from(chunk.files))
-          .filter(path => path.match(this.test));
-
-        for (const path of paths) {
-          const asset = compilation.assets[path];
-          const { source, map } = asset.sourceAndMap();
-          const postcssOpts = {
-            to: path,
-            from: path,
-            map: { prev: map || false }
-          };
-          const result = processCSS(source, postcssOpts);
-
-          if (result.map) {
-            compilation.assets[path] = new SourceMapSource(
-              result.css,
-              path,
-              JSON.parse(result.map),
-              source,
-              map,
-              true
+      if (isWebpack5) {
+        compilation.hooks.processAssets.tapPromise(
+          { name: NAME, stage: 'PROCESS_ASSETS_STAGE_OPTIMIZE' },
+          async assets => {
+            const paths = Object.keys(assets).filter(path =>
+              path.match(this.test)
             );
-          } else {
-            compilation.assets[path] = new RawSource(result.css);
+
+            for (const path of paths) {
+              const asset = compilation.assets[path];
+              const { source, map } = asset.sourceAndMap();
+              const postcssOpts = {
+                to: path,
+                from: path,
+                map: { prev: map || false }
+              };
+              const result = processCSS(source, postcssOpts);
+
+              if (result.map) {
+                compilation.assets[path] = new SourceMapSource(
+                  result.css,
+                  path,
+                  JSON.parse(result.map),
+                  source,
+                  map,
+                  true
+                );
+              } else {
+                compilation.assets[path] = new RawSource(result.css);
+              }
+            }
           }
-        }
-      });
+        );
+      } else {
+        compilation.hooks.optimizeChunkAssets.tapPromise(NAME, async chunks => {
+          const paths = Array.from(chunks)
+            .flatMap(chunk => Array.from(chunk.files))
+            .filter(path => path.match(this.test));
+
+          for (const path of paths) {
+            const asset = compilation.assets[path];
+            const { source, map } = asset.sourceAndMap();
+            const postcssOpts = {
+              to: path,
+              from: path,
+              map: { prev: map || false }
+            };
+            const result = processCSS(source, postcssOpts);
+
+            if (result.map) {
+              compilation.assets[path] = new SourceMapSource(
+                result.css,
+                path,
+                JSON.parse(result.map),
+                source,
+                map,
+                true
+              );
+            } else {
+              compilation.assets[path] = new RawSource(result.css);
+            }
+          }
+        });
+      }
     });
   }
 }
